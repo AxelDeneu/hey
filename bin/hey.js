3aa592ad68bbeb74e18f58cf92f622f1d50a560c
Axel DENEU <axel.deneu@gmail.com>
2018-06-05 17:04:09 +0200

    Hey! It's a simple patch

--- bin/hey.js
+++ bin/hey.js
@@ -3,6 +3,10 @@
 var hey = require('../lib/index.js');
 
 let args = process.argv.slice(2);
-let cmd = args.join(' ');
 
-hey.ho(cmd);
+if(args.length > 0) {
+	let cmd = args.join(' ');
+	hey.ho(cmd);
+} else {
+	console.log("Whoops! You must tell me the command you want to execute.");
+}
