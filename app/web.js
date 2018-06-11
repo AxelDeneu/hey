dec2eed1adfccdd278bd0dd83e3b24b5939ad45f
AxelDeneu <axel.deneu+1@gmail.com>
2018-06-11 20:16:28 +0200

    Hey! It's a patch to add a webGUI (Done)

--- app/web.js
+++ app/web.js
@@ -3,6 +3,8 @@ var app = express();
 var Datastore = require('nedb');
 var db = new Datastore({filename: __dirname+'/../data.db', autoload: true});
 var Twig = require('twig');
+const _ = require('lodash');
+var nl2br  = require('nl2br');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'twig');
@@ -11,11 +13,35 @@ app.set("twig options", {
 });
 
 app.get('/', (req, res) => {
-	db.find({}, (err, doc) => {
-		res.render('index.twig', {
-			message: "Hello World"
-		})
+	db.find({}).sort({time: -1}).exec((err, doc) => {
+		var grouped = _.groupBy(doc, (n) => {
+			return n.identifier;
+		});
+		_.forEach(grouped, (val, index) => {
+			grouped[index] = _.groupBy(val, (n) => {
+				return n.type;
+			})
+		});
+		res.render('index', {
+			data: grouped
+		});
 	})
 });
+
+app.get('/process/:identifier', (req, res) => {
+	db.find({identifier: req.params.identifier}).sort({time: 1}).exec((err, doc) => {
+		var grouped = _.groupBy(doc, (n) => {
+			return n.type
+		});
+		res.render('process', {
+			data: grouped,
+			nl2br: nl2br
+		});
+	})
+});
+
+app.get('/about', (req, res) => {
+	res.render('about');
+})
 
 app.listen(3000);
--- bin/hey.js
+++ bin/hey.js
@@ -5,8 +5,12 @@ var hey = require('../lib/index.js');
 let args = process.argv.slice(2);
 
 if(args.length > 0) {
-	let cmd = args.join(' ');
-	hey.ho(cmd);
+	if(args[0] == "web") {
+		hey.ho('node ' + __dirname + '/../app/web.js');
+	} else {
+		let cmd = args.join(' ');
+		hey.ho(cmd);
+	}
 } else {
 	console.log("Whoops! You must tell me the command you want to execute.");
 }
