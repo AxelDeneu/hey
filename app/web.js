c48ab795ac69462a94b64490fa20d5da2f1d79a7
Axel DENEU <axel.deneu@gmail.com>
2018-06-11 17:24:59 +0200

    Hey! It's a pacth to add a webGUI (WIP-3)

--- app/web.js
+++ app/web.js
@@ -2,11 +2,19 @@ var express = require('express');
 var app = express();
 var Datastore = require('nedb');
 var db = new Datastore({filename: __dirname+'/../data.db', autoload: true});
+var Twig = require('twig');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'twig');
+app.set("twig options", {
+	strict_variables: false
+});
 
 app.get('/', (req, res) => {
 	db.find({}, (err, doc) => {
-		console.log(err);
-		console.log(doc);
+		res.render('index.twig', {
+			message: "Hello World"
+		})
 	})
 });
 
